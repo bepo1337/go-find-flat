1fabc9d5cca75e576263919b030a192a99892abd
Benjamin Pöhlmann <benjamin_poehlmann@web.de>
2024-03-17 21:36:26 +0100

    add more postal codes to blacklist

--- validation.go
+++ validation.go
@@ -6,7 +6,7 @@ import "strings"
 var blacklistedPostalCodes = []int{
 	21035, 21037, // Allermöhe
 	22111, 22115, 22117, 22119, // Billstedt
-	22047, 22159, 22175, 22179, // Bramfeld
+	22047, 22159, 22175, 22177, 22179, 22309, // Bramfeld
 	22587,                             // Blankenese
 	22457, 22523, 22525, 22527, 22547, // Eidelstedt
 	21075, 21077, // Eißendorf
@@ -15,26 +15,29 @@ var blacklistedPostalCodes = []int{
 	22043, 22045, // Jenfeld
 	21077,                      // Marmstorf
 	21129,                      // Neuenfelde
-	22453,                      //Niendorf
+	22453, 22455, 22457, 22459, //Niendorf
 	22041,                      //Wandsbek
 	22549,                      // Osdorf
 	22391, 22393, 22395, 22399, //Poppenbüttel
-	21149,        //Hausbruch
-	22309,        // Steilshoop
-	22143, 22145, // Rahlstedt
+	21147, 21148, 21149, //Hausbruch
+	22309,                             // Steilshoop
+	22143, 22145, 22147, 22149, 22359, // Rahlstedt
 	21029,        //Altengamme/Bergedorf
 	22335,        //Fuhlsbüttel
 	22419, 22417, //Langenhorn
-	22339, //Hummelsbüttel
-	22049, // Dulsberg
-	22589, //Blankenese
-	22113, // Billbrook
-	22589, //Iserbrook
-	22559, //Rissen
-	22765, //Ottensen
-	21033, //Lohbrügge
-	22609, //Otmatschen
-
+	22339,                      //Hummelsbüttel
+	22049,                      // Dulsberg
+	22589,                      //Blankenese
+	22113,                      // Billbrook
+	22589,                      //Iserbrook
+	22559,                      //Rissen
+	22765,                      //Ottensen
+	21033,                      //Lohbrügge
+	22609,                      //Otmatschen
+	22397,                      //Duvenstedt
+	22761,                      //Bahrenfeld
+	22309, 22335, 22337, 22391, //Ohlsdorf
+	20535, //Hamm
 }
 
 func (app *Application) checkIfAdSuitable(ad Advertisement) bool {
@@ -43,7 +46,7 @@ func (app *Application) checkIfAdSuitable(ad Advertisement) bool {
 		return false
 	}
 
-	nameOK := app.checkName(strings.ToLower(ad.Name))
+	nameOK := app.checkName(ad.Name)
 	if !nameOK {
 		return false
 	}
@@ -53,15 +56,26 @@ func (app *Application) checkIfAdSuitable(ad Advertisement) bool {
 }
 
 func (app *Application) checkName(name string) bool {
-	if strings.Contains(name, "tauschwohnung") {
-		app.InfoLogger.Println("Didnt add ad because TAUSCHWOHNUNG")
+	name = strings.ToLower(name)
+
+	if nameContainsOne(name, "tausch") {
+		app.InfoLogger.Printf("Didnt add ad because TAUSCHWOHNUNG: %s\n", name)
 		return false
 	}
 
-	if strings.Contains(name, "untermiete") {
-		app.InfoLogger.Println("Didnt add ad because UNTERMIETE")
+	if nameContainsOne(name, "untermiete", "untervermietung", "zwischenmiete") {
+		app.InfoLogger.Printf("Didnt add ad because UNTERMIETE: %s\n", name)
 		return false
 	}
 
 	return true
+}
+
+func nameContainsOne(name string, words ...string) bool {
+	for _, word := range words {
+		if strings.Contains(name, word) {
+			return true
+		}
+	}
+	return false
 }
